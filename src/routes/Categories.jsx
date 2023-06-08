ce4524f87c94b2e56ef2b5325af6d49e29fcbd04
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-08 04:06:43 -0500

    Add SPAs

---
+++ src/routes/Categories.jsx
@@ -0,0 +1,7 @@
+const Categories = () => (
+  <div className="appBody">
+    <h2 className="top">Here goes Categories</h2>
+  </div>
+);
+
+export default Categories;
---
+++ src/routes/Home.jsx
@@ -0,0 +1,19 @@
+const Home = () => (
+  <div className="home">
+    <h2 className="top">Welcome to our page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Voluptas ab id repellendus, labore veniam fugit recusandae
+      porro non reprehenderit, maiores quos? Ipsam, minus voluptates
+      eaque esse earum recusandae voluptatem quia!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Voluptas ab id repellendus, labore veniam fugit recusandae
+      porro non reprehenderit, maiores quos? Ipsam, minus voluptates
+      eaque esse earum recusandae voluptatem quia!
+    </p>
+  </div>
+);
+
+export default Home;
