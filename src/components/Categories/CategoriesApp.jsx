548aee0739078369727da521a31c32c64b2c4300
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-13 07:21:29 -0500

    Add CategoriesApp skeleton

---
+++ src/components/Categories/CategoriesApp.jsx
@@ -0,0 +1,11 @@
+import CatHeader from 'components/Categories/CatHeader';
+import CategoryBody from 'components/Categories/CategoryBody';
+
+const CategoriesApp = () => (
+  <>
+    <CatHeader />
+    <CategoryBody />
+  </>
+);
+
+export default CategoriesApp;
