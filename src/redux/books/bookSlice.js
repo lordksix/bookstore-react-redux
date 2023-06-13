98f4510f19e8a38cdbd977f05f62147863a1358c
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-13 04:46:42 -0500

    Add initial books

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -2,7 +2,24 @@ import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
 const initialState = {
-  books: [],
+  books: [{
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  }],
 };
 
 const booksSlice = createSlice({
@@ -13,6 +30,7 @@ const booksSlice = createSlice({
       const newBook = {
         id: uuidv4(),
         title: action.playload.title,
+        author: action.playload.author,
         category: action.playload.category,
       };
       state.books = [...state.books, newBook];
