93a1ef05cc0c1a3823befedf7e97269631b59bc3
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-15 18:28:58 -0500

    Add API interactivity

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -3,58 +3,11 @@ import axios from 'axios';
 import { deselectCat, selectCat } from 'redux/categories/categoriesSlice';
 import { v4 as uuidv4 } from 'uuid';
 
-const GETBOOKS = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/qYH4IkyRgswDCxmNVl7g/books';
+const GETBOOKS = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/zd4f6JfPvZOyzdGBNV3e/books';
 
 const initialState = {
-  books: [{
-    item_id: uuidv4(),
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-    finishedChap: 16,
-    totalChap: 24,
-  },
-  {
-    item_id: uuidv4(),
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-    finishedChap: 2,
-    totalChap: 25,
-  },
-  {
-    item_id: uuidv4(),
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-    finishedChap: 0,
-    totalChap: 24,
-  }],
-  filteredBooks: [{
-    item_id: uuidv4(),
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-    finishedChap: 16,
-    totalChap: 24,
-  },
-  {
-    item_id: uuidv4(),
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-    finishedChap: 2,
-    totalChap: 25,
-  },
-  {
-    item_id: uuidv4(),
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-    finishedChap: 0,
-    totalChap: 24,
-  }],
-  booksAPI: [],
+  books: [],
+  filteredBooks: [],
   isLoading: false,
   error: undefined,
 };
@@ -62,30 +15,27 @@ const initialState = {
 export const getBooks = createAsyncThunk('books/getBooks', async (thunkAPI) => {
   try {
     const response = await axios.get(GETBOOKS);
-    console.log(response.data);
     return response.data;
   } catch (error) {
-    return thunkAPI.rejectWithValue({ error: error.message });
+    return thunkAPI.rejectWithValue('Error loading');
   }
 });
 
 export const postBooks = createAsyncThunk('books/postBooks', async (bookInfo, thunkAPI) => {
   try {
     const response = await axios.post(GETBOOKS, bookInfo);
-    console.log(response.data);
     return response.data;
   } catch (error) {
-    return thunkAPI.rejectWithValue({ error: error.message });
+    return thunkAPI.rejectWithValue('Error posting');
   }
 });
 
 export const deleteBooks = createAsyncThunk('books/deleteBooks', async (bookID, thunkAPI) => {
   try {
-    const response = await axios.post(`${GETBOOKS}/${bookID}`);
-    console.log(response.data);
+    const response = await axios.delete(`${GETBOOKS}/${bookID}`, { item_id: bookID });
     return response.data;
   } catch (error) {
-    return thunkAPI.rejectWithValue({ error: error.message });
+    return thunkAPI.rejectWithValue('Error deleting');
   }
 });
 
@@ -93,19 +43,30 @@ const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action) => {
-      const newBook = {
-        item_id: uuidv4(),
-        title: action.payload.title,
-        author: action.payload.author,
-        category: 'Non Classified',
-        finishedChap: 0,
-        totalChap: 24,
-      };
-      return ({ ...state, books: [...state.books, newBook] });
+    addBook: {
+      reducer(state, action) {
+        state.books.push(action.payload);
+        state.filteredBooks.push(action.payload);
+      },
+      prepare(title, author) {
+        return {
+          payload: {
+            item_id: uuidv4(),
+            title,
+            author,
+            category: 'Non Classified',
+            finishedChap: 0,
+            totalChap: 24,
+          },
+        };
+      },
     },
     removeBook: (state, action) => (
-      { ...state, books: [...state.books.filter((book) => book.item_id !== action.payload)] }
+      {
+        ...state,
+        books: [...state.books.filter((book) => book.item_id !== action.payload)],
+        filteredBooks: [...state.books.filter((book) => book.item_id !== action.payload)],
+      }
     ),
   },
   extraReducers: (builder) => {
@@ -118,34 +79,38 @@ const booksSlice = createSlice({
         ...state,
         filteredBooks: [...state.books],
       }))
-      .addCase(getBooks.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(getBooks.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload.error;
-      })
-      .addCase(postBooks.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(postBooks.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload.error;
-      })
-      .addCase(deleteBooks.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(deleteBooks.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload.error;
-      })
+      .addCase(getBooks.pending, (state) => ({
+        ...state,
+        isLoading: true,
+      }))
+      .addCase(getBooks.rejected, (state, action) => ({
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      }))
+      .addCase(postBooks.pending, (state) => ({
+        ...state,
+        isLoading: true,
+      }))
+      .addCase(postBooks.rejected, (state, action) => ({
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      }))
+      .addCase(deleteBooks.pending, (state) => ({
+        ...state,
+        isLoading: true,
+      }))
+      .addCase(deleteBooks.rejected, (state, action) => ({
+        state,
+        isLoading: false,
+        error: action.payload,
+      }))
       .addCase(getBooks.fulfilled, (state, action) => {
-        state.isLoading = false;
-        const booksAPI = Object.keys(action.payload).map((key) => {
+        const books = Object.keys(action.payload).map((key) => {
           const book = action.payload[key][0];
           return {
             item_id: key,
-            category: 'Non Classified',
             finishedChap: 0,
             totalChap: 24,
             ...book,
@@ -153,28 +118,33 @@ const booksSlice = createSlice({
         });
         return {
           ...state,
-          booksAPI,
+          isLoading: false,
+          books,
+          filteredBooks: books,
         };
       })
       .addCase(postBooks.fulfilled, (state, action) => {
-        state.isLoading = false;
         const newBook = {
-          id: action.meta.arg.item_id,
+          item_id: action.meta.arg.item_id,
           title: action.meta.arg.title,
           author: action.meta.arg.author,
           category: action.meta.arg.category,
           finishedChap: 0,
           totalChap: 24,
         };
-        return ({ ...state, booksAPI: [...state.books, newBook] });
-      })
-      .addCase(deleteBooks.fulfilled, (state, action) => {
-        state.isLoading = false;
         return {
           ...state,
-          booksAPI: [...state.booksAPI.filter((book) => book.item_id !== action.meta.arg)],
+          isLoading: false,
+          books: [...state.books, newBook],
+          filteredBooks: [...state.books, newBook],
         };
-      });
+      })
+      .addCase(deleteBooks.fulfilled, (state, action) => ({
+        ...state,
+        isLoading: false,
+        books: [...state.books.filter((book) => book.item_id !== action.meta.arg)],
+        filteredBooks: [...state.books.filter((book) => book.item_id !== action.meta.arg)],
+      }));
   },
 });
 
@@ -182,7 +152,6 @@ export const {
   addBook, removeBook,
 } = booksSlice.actions;
 
-export const selectBooks = (state) => state.books.books;
-export const selectFilteredBooks = (state) => state.books.filteredBooks;
+export const selectBooks = (state) => state.books;
 
 export default booksSlice.reducer;
