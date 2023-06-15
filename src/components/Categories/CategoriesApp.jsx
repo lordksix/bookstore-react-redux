ba74f0f797212025f8ee16d93e45278c942f7222
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-15 03:28:08 -0500

    Update CategoriesApp.jsx

--- src/components/Categories/CategoriesApp.jsx
+++ src/components/Categories/CategoriesApp.jsx
@@ -1,11 +1,53 @@
-import CatHeader from 'components/Categories/CatHeader';
-/* import CategoryBody from 'components/Categories/CategoryBody'; */
+import { v4 as uuidv4 } from 'uuid';
+import styles from 'styles/Categories/CategoriesApp.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectFilteredBooks, filterBooks, unfilterBooks } from 'redux/books/bookSlice';
+import {
+  selectCategories,
+} from 'redux/categories/categoriesSlice';
+import CategoryBody from './CategoryBody';
 
-const CategoriesApp = () => (
-  <>
-    <CatHeader />
-    {/* <CategoryBody /> */}
-  </>
-);
+const CategoriesApp = () => {
+  const categories = useSelector(selectCategories);
+  const filteredBooks = useSelector(selectFilteredBooks);
+  const dispatch = useDispatch();
+
+  const handleSelect = (e) => {
+    const id = e.target.textContent;
+    dispatch(filterBooks(id));
+  };
+  const handleDeselect = () => {
+    dispatch(unfilterBooks());
+  };
+  const deselectOption = (
+    <li key={uuidv4()}>
+      <button type="button" className={styles.noBtn} onClick={() => handleDeselect()}>
+        None
+      </button>
+    </li>
+  );
+  const catHeaders = categories.map((cat) => (
+    <li key={uuidv4()}>
+      <button type="button" className={styles.noBtn} onClick={(e) => handleSelect(e)}>
+        {cat}
+      </button>
+    </li>
+  ));
+
+  return (
+    <>
+      <div className={styles.catHeader}>
+        <h2>Book Category</h2>
+        <nav className="navbar">
+          <ul>
+            {catHeaders}
+            {deselectOption}
+          </ul>
+        </nav>
+      </div>
+      <CategoryBody filteredBooks={filteredBooks} />
+    </>
+  );
+};
 
 export default CategoriesApp;
